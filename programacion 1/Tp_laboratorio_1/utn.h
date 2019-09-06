3d7b43d9e5a68bc2cf8280adbe551686cf8f9e8f
fumi1993 <47338032+fumi1993@users.noreply.github.com>
2019-09-06 14:31:51 -0300

    Add files via upload

---
+++ programacion 1/Tp_laboratorio_1/utn.h
@@ -0,0 +1,8 @@
+//estos son los prototipos utilizados en las funciones.
+
+float suma(float A, float B);
+float resta(float A, float B);
+float multiplicacion(float A, float B);
+float division(float A, float B);
+float factorial(float A, float B);
+
